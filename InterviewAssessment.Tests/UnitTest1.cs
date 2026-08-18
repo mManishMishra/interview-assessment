8a0c0b006a2bb0439b9d843411824786e71b1a16
unknown <nareshmahi957@gmail.com>
2026-08-18 19:46:12 +0530

    practiced failed and passed test using Assert methods

---
+++ InterviewAssessment.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace InterviewAssessment.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void StartActivity_WithAssemblyActivity_CallsDriver()
+    {
+        Assert.Fail("This test is a placeholder. Please implement the test logic here.");
+        // Assert.True(true);
+    }
+}
